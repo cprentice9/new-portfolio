505002a6d35bb9cc38d90dbf824f9eababba5217
Connor Prentice <connurp@gmail.com>
2022-11-07 17:31:09 -0600

    added real text to the about page instead of placeholder

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,14 +14,15 @@ function About() {
           src="https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/311435266_10228022015446481_375629265674883144_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qhbQQFGym1YAX-gGgak&_nc_ht=scontent-dfw5-2.xx&oh=00_AfD_ajiaa9wln9NWZsqbFAhDvB4_s6mARLPX1tmHVRi5Vw&oe=636F4CFD"
         />
         <p className="about_paragraph">
-          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
-          atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
-          sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
-          facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-          impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-          repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et
-          voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-          ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+          After going through three years of University and having my first child with my wife, I decided that I wanted
+          a change in my life. In early 2022 I started the Dev Mountain Full Stack Web Dev Program. I completed the
+          program a month ahead of schedule in September of 2022. I am currently working as a Software Engineer II at
+          Armada Power. I am currently living in North Texas where I have lived with my family for the last 6 years. I
+          grew up in the Bay Area, in a suburb of San Francisco called Danville. I went to school from K-12 in Danville.
+          I attended K-5 at Vista Grande Elementary School. I attended grades 6-8 at Los Cerros Middle School. I
+          attended all four years of High School at Monte Vista High School. In High School I played third base for the
+          baseball team. I did three years of a BS in Physics at the University of North Texas. I am a die hard Yankees
+          fan and will be until I leave this earth. GO YANKS!
         </p>
       </div>
     </div>
