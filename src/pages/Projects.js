84c72b44bd5006701adc55c03884ef37d06e981c
Connor Prentice <connurp@gmail.com>
2022-11-07 17:42:13 -0600

    changed the bootstrap cards on the projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Projects.css";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
 
 function Home() {
   return (
@@ -9,21 +8,14 @@ function Home() {
       <h1 className="my_work">My Work</h1>
       <div className="projects_container">
         <Card style={{ width: "18rem" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
           <Card.Body>
             <Card.Title>Amazon Clone</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              React, Node, Express, Firebase Cloud Functions, Stripe API
+            </Card.Subtitle>
             <Card.Text>
               I built a working Amazon Clone with React that has payment processing built in through the stripe API.
             </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>React</ListGroup.Item>
-            <ListGroup.Item>Node</ListGroup.Item>
-            <ListGroup.Item>Express</ListGroup.Item>
-            <ListGroup.Item>Firebase Cloud Functions</ListGroup.Item>
-            <ListGroup.Item>Stripe Payment API</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
             <Card.Link href="https://amazon-clone.coolestconnor.com/">Link to App</Card.Link>
             <Card.Link href="https://github.com/cprentice9/Devmountain/tree/main/amazonCloneFinalProject/amazon-clone">
               GitHub Repo
@@ -31,19 +23,13 @@ function Home() {
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
           <Card.Body>
             <Card.Title>Random Movie Picker</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">React, IMDb 250 Most Popular Movies API</Card.Subtitle>
             <Card.Text>
               I built a Random Movie Picker app with React that connects to the IMdB 250 Most Popular Movies API and
               picks a random movie from the pool for you to watch.
             </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>React</ListGroup.Item>
-            <ListGroup.Item>IMDb 250 Most Popular Movies API</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
             <Card.Link href="https://random-movie-picker-assessment.herokuapp.com/">Link to App</Card.Link>
             <Card.Link href="https://github.com/cprentice9/random-movie-picker">GitHub Repo</Card.Link>
           </Card.Body>
